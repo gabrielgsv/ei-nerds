1a5c86164f1406e517a9e2866e360231c5ef2cd4
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-06-04 18:38:35 -0300

    added get credits and get providers functions

--- app/service.ts
+++ app/service.ts
@@ -15,5 +15,55 @@ export function getPopularMovies() {
     })
     .catch((error) => {
       console.error(error);
+      return [];
+    });
+}
+
+export function getCreditsMovie(movieId: number) {
+  return api
+    .get(`movie/${movieId}/credits`, {
+      params: {
+        language: "pt-BR",
+      },
+    })
+    .then((response) => {
+      return response.data.cast;
+    })
+    .catch((error) => {
+      console.error(error);
+      return [];
+    });
+}
+
+export function getProviderMovie(movieId: number) {
+  type ProviderMovie = {
+    BR: {
+      flatrate: [];
+      rent: [];
+    };
+  };
+
+  return api
+    .get(`movie/${movieId}/watch/providers`, {
+      params: {
+        language: "pt-BR",
+      },
+    })
+    .then((response) => {
+      const providerMovie: ProviderMovie = response.data.results;
+      const { BR } = providerMovie;
+
+      if (BR) {
+        const flatrate = BR.flatrate || [];
+        const rent = BR.rent || [];
+
+        return [...flatrate, ...rent];
+      }
+
+      return [];
+    })
+    .catch((error) => {
+      console.error(error);
+      return [];
     });
 }
