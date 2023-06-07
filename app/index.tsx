fae552fa739889c9bba1f7bff11cc37ef9b36fa1
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-06-07 14:51:45 -0300

    fix setMovies with oldValue and separate list component

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
 import { Dimensions } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
-import { Spinner, YStack } from "tamagui";
+import { YStack } from "tamagui";
 
 import Header from "../components/Header";
-import PanelCard from "../components/PanelCard";
+import List from "../components/List";
 import SearchInput from "../components/SearchInput";
 import { SelectType } from "../store/useSelectContent";
 
@@ -16,6 +15,8 @@ export default function Home() {
   const [totalPage, setTotalPage] = useState(1);
   const [search, setSearch] = useState("");
 
+  const { width } = Dimensions.get("window");
+
   useEffect(() => getMovies(1), []);
 
   function getMovies(newPage: number) {
@@ -25,14 +26,13 @@ export default function Home() {
       if (search.length > 0 && newPage === 1) {
         setMovies(res.results);
       } else {
-        setMovies([...movies, ...res.results]);
+        setMovies((value) => [...value, ...res.results]);
       }
       setTotalPage(res.total_pages);
     });
   }
 
   const getNumColumns = () => {
-    const { width } = Dimensions.get("window");
     const numColumns = Math.floor(width / 144);
     return numColumns;
   };
@@ -41,26 +41,22 @@ export default function Home() {
     <>
       <Header title="Filmes" />
 
-      <YStack m="$4" als="center">
+      <YStack m="$4" als="center" width={width} px="$4">
         <SearchInput
           size="$3"
           search={search}
           setSearch={setSearch}
           getMovies={getMovies}
           setMovies={setMovies}
+          setPage={setPage}
         />
 
-        <FlatList
+        <List
           data={movies}
-          keyExtractor={(item) => item?.id?.toString()}
-          renderItem={(item) => <PanelCard item={item.item} />}
-          showsVerticalScrollIndicator={false}
-          numColumns={getNumColumns()}
-          onEndReached={() => getMovies(page + 1)}
-          onEndReachedThreshold={0.1}
-          ListFooterComponent={() =>
-            page < totalPage && <Spinner size="large" color="$gray11" />
-          }
+          getData={getMovies}
+          getNumColumns={getNumColumns}
+          page={page}
+          totalPage={totalPage}
         />
       </YStack>
     </>
