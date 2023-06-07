18ac27e8f0cf31e691f93eddd1640550eee291d1
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-06-07 14:50:17 -0300

    separated flatList in a component

---
+++ components/List.tsx
@@ -0,0 +1,39 @@
+import { FlatList } from "react-native-gesture-handler";
+import { Spinner } from "tamagui";
+
+import { SelectType } from "../store/useSelectContent";
+
+import PanelCard from "./PanelCard";
+
+type Props = {
+  data: SelectType[];
+  page: number;
+  totalPage: number;
+  getNumColumns: () => number;
+  getData: (page: number) => void;
+};
+
+export default function List({
+  data,
+  page,
+  totalPage,
+  getNumColumns,
+  getData,
+}: Props) {
+  return (
+    <>
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={(item) => <PanelCard item={item.item} />}
+        showsVerticalScrollIndicator={false}
+        numColumns={getNumColumns()}
+        onEndReached={() => getData(page + 1)}
+        onEndReachedThreshold={0.5}
+        ListFooterComponent={() =>
+          page < totalPage && <Spinner size="large" color="$gray11" />
+        }
+      />
+    </>
+  );
+}
