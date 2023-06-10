eb6352c6b44a0a79b16a468705558a8af05f75e4
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-06-10 00:01:10 -0300

    fix example

--- utils/example.env.ts
+++ utils/example.env.ts
@@ -1,3 +1,3 @@
 export const API_TOKEN = "";
-export const API_URL = "";
-export const API_IMAGE_URL = "";
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
