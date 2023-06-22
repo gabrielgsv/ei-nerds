d889d2bc9edf652768aeff6e30d79e713b7b0d94
Gabriel Soares Vilela <39222640+gabrielgsv@users.noreply.github.com>
2023-06-22 16:05:34 -0300

    Delete env.ts

--- utils/env.ts
+++
@@ -1,4 +0,0 @@
-export const API_TOKEN =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmFhZDYyNzMwODc4M2YwMTJlNDE4NTJmYTk4ZTZjZCIsInN1YiI6IjY0NzY4NTljMWY5OGQxMDI4NzdkZmVjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n5LVNCqWrQcMIpsrXc50ioJkRonysaES_pqzSlvuJkA";
-export const API_URL = "https://api.themoviedb.org/3/";
-export const API_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
